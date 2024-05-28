48988951b4b076b028d067de7b894438f84f9812
Inokcode <regilearn@gmail.com>
2024-05-28 18:20:33 +0530

    getuserbyId route added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express,{Express} from 'express';
 import mongoose from 'mongoose';
+import FinanceRecordRouter from "./routes/financialrecords";
+
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
@@ -10,5 +12,7 @@ const mongoURI: string = "mongodb+srv://regilearn:3XYLkpVhyABZLywA@personalfinan
 mongoose.connect(mongoURI)
 .then(()=> console.log("CONNECTED TO MONGODB!"))
 .catch((err)=> console.error("FAILED TO CONNECT TO MONGODB:", err));
+
+app.use("/financialrecords",FinanceRecordRouter);
 
 app.listen(port,()=> console.log(`Server Running on Port ${port}`))
---
+++ server/src/routes/financialrecords.ts
@@ -0,0 +1,22 @@
+import express,{Request,Response} from "express";
+import FinanceRecordModel from "../schema/financialrecord";
+
+
+const router = express.Router();
+
+router.get("/getAllByUserID", async (req:Request,res:Response)=>{
+    try {
+        const userId = req.params.userId;
+        const records = await FinanceRecordModel.find({userId:userId});
+        if(records.length === 0){
+            return res.status(404).send("No records found for the user")
+        }
+        res.status(200).send(records);
+    } catch (err) {
+      res.status(500).send(err)  
+    }
+});
+
+
+
+export default router;
